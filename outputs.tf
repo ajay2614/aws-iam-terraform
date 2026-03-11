bf49dcdece5a8162b81bad6bea66e792e435af85
Ajay <86017484+ajay2614@users.noreply.github.com>
2026-03-11 16:56:55 +0530

    terraform output comment update

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 // Outputs are printed after `terraform apply`.
 output "iam_role_name" {
   // The actual IAM role name created in AWS.
+  // aws_iam_role = resource type, iam_role = local alias, name = AWS role name.
   value       = aws_iam_role.iam_role.name
   description = "Name of the IAM role created by Terraform."
 }
